cdcac7757924a2cf646a5dc81a90373c92dbfe56
LagBot5 <167463120+LagBot5@users.noreply.github.com>
2024-09-06 09:22:37 -0400

    Create Quadratic.js

---
+++ Quadratic.js
@@ -0,0 +1,22 @@
+function setup() {
+let a = Math.round(random(1, 10));
+let b = Math.round(random(1, 10));
+let c = Math.round(random(1, 10));
+print (a + "x^2 + " + b + "x + " + c);
+
+let d = Math.pow(b, 2) - 4 * a * c;
+print ("discriminant = " + d);
+
+let discriminant = Math.sqrt(d);
+	
+if (d < 0){
+print ("not possible");
+}
+	else {
+		print ("possible");
+		let r1 = (-b + discriminant)/(2 * a);
+		print ("adding: " + r1);
+		let r2 = (-b - discriminant)/(2 * a);
+		print ("subtracting: " + r2);
+}
+}
